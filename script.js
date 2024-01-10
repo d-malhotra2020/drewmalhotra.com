d0beae190f64923107038b0bbd91dab051919f46
Drew Malhotra <dhruvmalhotra2023@gmail.com>
2024-01-10 14:07:15 -0600

    Added CSS and JS pages

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Page loaded and script running");
+    // You can add interactive features or additional JavaScript here
+});
