8a84501b354ef003d25c952d67e9e69f0bcea980
Drew Malhotra <dhruvmalhotra2023@gmail.com>
2024-01-10 14:23:34 -0600

    Added CSS and JS pages. Removed JS page for time being

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("Page loaded and script running");
-    // You can add interactive features or additional JavaScript here
-});
