c418550544d53c5a604b7ba8a0310a5887820319
Drew Malhotra <dhruvmalhotra2023@gmail.com>
2024-01-12 08:02:01 -0600

    added some basic portfolio architecture

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const { JSDOM } = require("jsdom");
-const { document } = (new JSDOM(``)).window;
-// Now you can use 'document' as if you were in a browser
+document.addEventListener('DOMContentLoaded', function() {
+    var projects = [
+        { name: 'Project 1', description: 'Description of project 1', link: 'http://link-to-project1.com' },
+        // Add more projects here
+    ];
+
+    var projectsContainer = document.getElementById('projects');
+    projects.forEach(function(project) {
+        var projectElement = document.createElement('div');
+        projectElement.innerHTML = `
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <a href="${project.link}" target="_blank">View Project</a>
+        `;
+        projectsContainer.appendChild(projectElement);
+    });
+});
