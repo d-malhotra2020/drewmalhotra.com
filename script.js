22820ca5594bbd561047d6adad259bafdab7a1a4
Drew Malhotra <dhruvmalhotra1986@gmail.com>
2024-01-17 11:08:18 -0600

    added background effect

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var projects = [
-        // { name: 'Project 1', description: 'Description of project 1', link: 'http://link-to-project1.com' },
-        // Add more projects here
-    ];
+    var video = document.querySelector('video');
 
-    var projectsContainer = document.getElementById('projects');
-    projects.forEach(function(project) {
-        var projectElement = document.createElement('div');
-        projectElement.innerHTML = `
-            <h3>${project.name}</h3>
-            <p>${project.description}</p>
-            <a href="${project.link}" target="_blank">View Project</a>
-        `;
-        projectsContainer.appendChild(projectElement);
+    // Example: Pause video on click
+    video.addEventListener('click', function() {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
     });
 });
