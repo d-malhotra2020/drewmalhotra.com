c750813fc8d0e2bdbdaf2d5f1c4956137e51144c
Drew Malhotra <dhruvmalhotra1986@gmail.com>
2024-01-19 10:07:53 -0600

    added more videos in the background

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var video = document.querySelector('video');
+    var videoPlayer = document.getElementById('videoPlayer');
+    var videos = ['rocket_launch.mp4', 'satellite.mp4', 'CIA.mp4', 'hubble_telescope.mp4', 'codingprogramming.mp4', 'coding.mp4', 'algorithm.mp4'];
+    var currentVideoIndex = 0;
 
-    // Example: Pause video on click
-    video.addEventListener('click', function() {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
+    videoPlayer.src = videos[currentVideoIndex]; // Set initial source
+    videoPlayer.play(); // Play the first video
+
+    videoPlayer.addEventListener('ended', function() {
+        currentVideoIndex++; // Move to the next video
+        if (currentVideoIndex >= videos.length) {
+            currentVideoIndex = 0; // Reset to first video if at the end of the array
         }
+        videoPlayer.src = videos[currentVideoIndex]; // Change the video source
+        videoPlayer.play(); // Play the video
     });
 });
